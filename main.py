74c8b58483b62d6bda2a1907c3c0c09be197bad6
Richie <richardokonicha@gmail.com>
2022-09-28 13:18:36 +0100

    getting playlist

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
-"""
-Use a playlist to stream files from m3u8: https://github.com/globocom/m3u8
-    pip install m3u8
-Example from Akamai:
-https://learn.akamai.com/en-us/webhelp/media-services-live/media-services-live-4-user-guide/GUID-0A50253F-0B1B-406B-A8C9-3788CB42F950.html
-"""
-
+import os
+import ssl
 from pathlib import Path
 import pylivestream.api as pls
-
-import m3u8
-
-playlist_m3u8 = Path(__file__).parent / "local.m3u8"
-key = Path(__file__).parent / "youtube.key"
+from pytube import YouTube, Playlist
 
 
-playlist = m3u8.load(str(playlist_m3u8))
+def start():
+  ssl._create_default_https_context = ssl._create_unverified_context
+  playlist_string = 'https://youtube.com/playlist?list=OLAK5uy_mDWtgeNF9LzujhjNi5DFcX-Lld0_ZMr_s'
+  urlm = "rtmp://a.rtmp.youtube.com/live2"
+  key = Path(__file__).parent / "youtube.key"
 
-files = playlist.files
+  playlist = Playlist(playlist_string)
+  print("start....")
+  sites = ["youtube"]
+  while True:
+    for video in playlist.videos:
+        videofile = video.streams.get_highest_resolution().download()
+        print("stream....")
+        pls.stream_file(
+            ini_file=key,
+            websites=sites,
+            assume_yes=True,
+            video_file=videofile,
+        )
 
-sites = ["youtube"]
-urlm = "rtmp://a.rtmp.youtube.com/live2"
-# while True:
-for file in files:
-    pls.stream_file(
-        loop=True,
-        ini_file=key,
-        websites=sites,
-        assume_yes=True,
-        video_file=file,
-    )
+if __name__ == "__main__":
+  start()
--- screen.py
+++
@@ -1,7 +0,0 @@
-"""
-example of screen sharing to livestream
-"""
-
-import pylivestream.api as pls
-
-pls.stream_screen(ini_file=None, websites="localhost", assume_yes=True)
